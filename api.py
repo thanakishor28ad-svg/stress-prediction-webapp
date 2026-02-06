28ed4d80e677ec1d09cc89144b33972942c73f12
thanakishor28ad-svg <thanakishor.28ad@licet.ac.in>
2026-02-06 11:07:17 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,29 +1,25 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import joblib
 import pandas as pd
+import joblib
 
 app = FastAPI()
 
 model = joblib.load("stress_model.pkl")
-
-class StressInput(BaseModel):
-    work_hours: int
-    sleep_hours: int
-    heart_rate: int
 
 @app.get("/")
 def home():
     return {"message": "Stress Prediction API is running"}
 
 @app.post("/predict")
-def predict(data: StressInput):
+def predict(data: dict):
+
     df = pd.DataFrame([{
-        "work_hours": data.work_hours,
-        "sleep_hours": data.sleep_hours,
-        "heart_rate": data.heart_rate
+        "WorkHours": data["WorkHours"],
+        "SleepHours": data["SleepHours"],
+        "HeartRate": data["HeartRate"],
+        "ScreenTime": data["ScreenTime"],
+        "ExerciseHours": data["ExerciseHours"]
     }])
 
     prediction = model.predict(df)[0]
     return {"stress_level": int(prediction)}
-
